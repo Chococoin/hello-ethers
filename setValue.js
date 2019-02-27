7feafa6c082da36766e4092cda7296029d46af9a
Germán Lugo <german.lugo@bitnibs.com>
2019-02-28 00:16:05 +0100

    Working version with infura.

--- setValue.js
+++ setValue.js
@@ -1,10 +1,9 @@
 var ethers = require('ethers');
 const ABI = require('./build/contracts/HelloWorld.json')
 
-const provider = new ethers.providers.JsonRpcProvider('http://localhost:7545');
+const provider = new ethers.providers.InfuraProvider('ropsten', '13eb2f7a24d048379237d1a85912203c');
 
-//const address  = '0x1eA5981E127b4AEfbFAaae48c6B7380351144519';
-const address  = ABI.networks[5777].address;
+const address  = ABI.networks[3].address;
 const abi = ABI.abi;
 
 const privKey = '826d281202a5f75190b589dba6af76778f3b479f3d2895fe7c3f74c232d15013';
