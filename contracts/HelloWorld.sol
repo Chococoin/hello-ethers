f0f39d437599fc46dffe2393421b265e46160674
Germán Lugo <german.lugo@bitnibs.com>
2019-02-27 22:49:51 +0100

    Compile ready.

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -8,12 +8,12 @@ contract HelloWorld {
     	name = "Hola Mundo!";
     }
     
-    function setValue(string memory _name) returns (string){
+    function setValue(string memory _name) public returns (string memory){
         name = _name;
         return name;
     }
     
-    function getValue() public constant returns (string){
+    function getValue() public view returns (string memory){
         return name;
     }
     
